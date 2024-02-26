71fbd2473791cda4a49bd88fb12bea2b372d0080
seniel98 <jdpadron98@gmail.com>
2024-02-26 13:25:48 +0100

    Added the possibility to select point based on kriging file

--- real_traffic_distribution_model/traffic_model/routes_generator.py
+++ real_traffic_distribution_model/traffic_model/routes_generator.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from geopy.point import Point
 import json
 from numpy.random import randint
+import ast
 
 sys.path.append("/home/josedaniel/real_traffic_distribution_model")
 
@@ -79,11 +80,42 @@ def filter_not_suitable_edges(net, roundabouts):
     not_suitable_edges = []
     not_suitable_edges_set = set()
     for edge in net.getEdges():
-        if edge.getID() in roundabouts or edge.getLength() < 50 or edge.getType() == "highway.primary" or edge.getType() == "highway.primary_link" or edge.getType() == "highway.track" or edge.getType() == "highway.motorway_link":
+        if edge.getID() in roundabouts or edge.getLength() < 50 or edge.getType() == "highway.primary_link" or edge.getType() == "highway.track" or edge.getType() == "highway.motorway_link":
             not_suitable_edges.append(str(edge.getID()))
 
     not_suitable_edges_set.update(not_suitable_edges)
     return not_suitable_edges_set
+
+
+def filter_suited_edges(net, roundabouts):
+    suited_rows = []  # List to hold all rows of edge_id and coord_node pairs
+    for edge in net.getEdges():
+        if edge.getID() not in roundabouts and edge.getLength() > 50 and edge.getType() != "highway.primary_link" and edge.getType() != "highway.track" and edge.getType() != "highway.motorway_link":
+            edge_id = str(edge.getID())
+            for coord in edge.getRawShape():
+                # Convert the coordinates to lat, lon
+                lon, lat = net.convertXY2LonLat(coord[0], coord[1])
+                # Round the coordinates to 5 decimal places
+                lat = round(lat, 4)
+                lon = round(lon, 4)
+                suited_rows.append({'edge_id': edge_id, 'coord_node': (lat, lon)})
+            for coord in edge.getShape():
+                # Convert the coordinates to lat, lon
+                lon, lat = net.convertXY2LonLat(coord[0], coord[1])
+                # Round the coordinates to 5 decimal places
+                lat = round(lat, 4)
+                lon = round(lon, 4)
+                suited_rows.append({'edge_id': edge_id, 'coord_node': (lat, lon)})
+            # Append from node
+            x, y = edge.getFromNode().getCoord()
+            lon, lat = net.convertXY2LonLat(x, y)
+            suited_rows.append({'edge_id': edge_id, 'coord_node': (round(lat, 4), round(lon, 4))})
+            # Append to node
+            x, y = edge.getToNode().getCoord()
+            lon, lat = net.convertXY2LonLat(x, y)
+            suited_rows.append({'edge_id': edge_id, 'coord_node': (round(lat, 4), round(lon, 4))})
+
+    return pd.DataFrame(suited_rows)
 
 
 def edge_to_coordinates(edge, net):
@@ -115,6 +147,64 @@ def select_district(df, src_district=None):
     n_vehicles_prob = np.true_divide(abs_n_vehicles, total_vehicles)
     selected_district = np.random.choice(district_list, 1, p=n_vehicles_prob)
     return selected_district[0]
+
+
+def filter_distance(df, src_point, src_district=None):
+    df_coord_filtered = df.copy()
+
+    for index, row in df.iterrows():
+        # 1,25 km is based on the assumption that a person walks 1,25 km in 15 minutes and in bicycle 1,25 km in 6 minutes
+        if geodesic(src_point, eval(row['coord_node'])).m < 1250:
+            df_coord_filtered.drop(index, axis=0, inplace=True)
+        # # Drop all the nodes that are in the same district as the source
+        # if src_district and row['district_code'] == src_district:
+        #     df_coord_filtered.drop(index, axis=0, inplace=True)
+
+    if len(df_coord_filtered) > 0:
+        return True
+
+
+def select_point_from_kriging(df):
+    coord_node_list = df['coord_node'].to_list()
+    n_vehicles = df['n_vehicles'].to_list()
+    total_vehicles = np.sum(n_vehicles)
+    n_vehicles_prob = np.true_divide(n_vehicles, total_vehicles)
+    selected_point = np.random.choice(coord_node_list, 1, p=n_vehicles_prob)
+    return eval(selected_point[0])
+
+
+def select_origin_destination_from_kriging(options, kriging_df, veh_per_district_df, is_dest=False, src_point=None,
+                                           src_district=None):
+    df_district_filtered = kriging_df.copy()
+
+    # Select district
+    district = select_district(veh_per_district_df, src_district=src_district)
+
+    # Filter the dataframe by the selected district
+    df_district_filtered = df_district_filtered[df_district_filtered['district_code'] == district]
+    if not is_dest:
+
+        selected_point = select_point_from_kriging(df_district_filtered)
+        ata = get_ATA_from_db(sqlite3.connect(options.traffic_db),
+                              rtdm.coord_2_node(sqlite3.connect(options.dbPath), selected_point[0], selected_point[1]))
+    else:
+        # Selecting destination
+        df_coord_filtered = df_district_filtered.copy()
+
+        for index, row in df_district_filtered.iterrows():
+            # 1,25 km is based on the assumption that a person walks 1,25 km in 15 minutes and in bicycle 1,25 km in 6 minutes
+            if geodesic(src_point, eval(row['coord_node'])).m < 1250:
+                df_coord_filtered.drop(index, axis=0, inplace=True)
+
+        if len(df_coord_filtered) > 0:
+            selected_point = select_point_from_kriging(df_coord_filtered)
+            ata = get_ATA_from_db(sqlite3.connect(options.traffic_db),
+                                  rtdm.coord_2_node(sqlite3.connect(options.dbPath), selected_point[0],
+                                                    selected_point[1]))
+        else:
+            return None, None, None
+
+    return ata, selected_point, district
 
 
 def select_origin_destination(options, df, veh_per_district_df, is_dest=False, src_point=None, src_district=None):
@@ -150,6 +240,26 @@ def select_origin_destination(options, df, veh_per_district_df, is_dest=False, s
     return ata, point, district
 
 
+def prepare_kriging_df(df):
+    # Convert string representations of tuples in 'coord_node' to actual tuples
+    df['coord_node'] = df['coord_node'].apply(ast.literal_eval)
+
+    # Round the coordinates to 6 decimal places
+    df['coord_node'] = df['coord_node'].apply(lambda x: (round(x[0], 4), round(x[1], 4)))
+
+    # Join the duplicate the rows with the same coordinates
+    df = df.groupby('coord_node').agg(
+        {'n_vehicles': 'mean', 'district_code': 'first'}).reset_index()
+
+    # Make n_vehicles an integer
+    df['n_vehicles'] = df['n_vehicles'].astype(int)
+
+    # Make sure coord node is a string
+    df['coord_node'] = df['coord_node'].astype(str)
+
+    return df
+
+
 def create_od_routes(options, net):
     """
     It takes a traffic file and a traffic database, and generates a
@@ -162,17 +272,11 @@ def create_od_routes(options, net):
 
     traffic_df = pd.read_csv(options.traffic_file)
 
-    veh_per_district_df = pd.read_csv(options.veh_per_district)
+    # veh_per_district_df = pd.read_csv(options.veh_per_district)
 
-    # veh_per_district_df['geometry'] = veh_per_district_df['geo_shape'].apply(parse_polygon)
-    #
-    # # Filter out rows with invalid geometries
-    # veh_per_district_gdf = gpd.GeoDataFrame(veh_per_district_df, geometry='geometry')
-    #
-    # traffic_df['geometry'] = traffic_df['coord_node'].apply(parse_point)
-    #
-    # # Filter out rows with invalid geometries
-    # traffic_gdf = gpd.GeoDataFrame(traffic_df.dropna(subset=['geometry']), geometry='geometry')
+    kriging_ata_df = pd.read_csv(options.kriging_ata_file)
+
+    kriging_ata_df = prepare_kriging_df(kriging_ata_df)
 
     roundabouts = set()
 
@@ -208,13 +312,33 @@ def create_od_routes(options, net):
 
     not_suitable_edges = filter_not_suitable_edges(net, roundabouts)
 
+    # Use the modified function to get the DataFrame
+    suitable_edges_df = filter_suited_edges(net, roundabouts)
+
+    # Make sure coord_node is a string
+    suitable_edges_df['coord_node'] = suitable_edges_df['coord_node'].astype(str)
+
+    # Join the kriging_ata_df with the suitable_edges_df
+    kriging_ata_df = kriging_ata_df.merge(suitable_edges_df, on='coord_node', how='inner')
+
+    # Drop the duplicates on coord_node
+    kriging_ata_df.drop_duplicates(subset='coord_node', inplace=True)
+
+    kriging_ata_df.reset_index(drop=True, inplace=True)
+
+    # kriging_ata_df.to_csv("/home/josedaniel/Algoritmo_rutas_eco/kriging_ata_df.csv", index=False)
+
+    # Aggregate the number of vehicles per district
+    veh_per_district_df = kriging_ata_df.groupby('district_code').agg({'n_vehicles': 'sum'}).reset_index()
+
     # The above code is generating routes for the given percentage of vehicles.
     while total_vehicles > int((total_vehicles_copy * (1 - (percentage / 100)))):
 
         if not is_reiterating:
             exec_time_start = datetime.now()
 
-        src_ata, src_point, src_district = select_origin_destination(options, traffic_df, veh_per_district_df)
+        src_ata, src_point, src_district = select_origin_destination_from_kriging(options, kriging_ata_df,
+                                                                                  veh_per_district_df)
         if src_point is None:
             is_reiterating = True
             continue
@@ -222,9 +346,10 @@ def create_od_routes(options, net):
         # Make sure column coord_node is a string
         traffic_df['coord_node'] = traffic_df['coord_node'].astype(str)
 
-        des_ata, des_point, des_district = select_origin_destination(options, traffic_df, veh_per_district_df,
-                                                                     is_dest=True,
-                                                                     src_point=src_point)
+        des_ata, des_point, des_district = select_origin_destination_from_kriging(options, kriging_ata_df,
+                                                                                  veh_per_district_df, is_dest=True,
+                                                                                  src_point=src_point,
+                                                                                  src_district=src_district)
 
         if des_point is not None:
             coords_route, nodes_route, edges_route = calculate_route(src_point, des_point, net, not_suitable_edges)
@@ -232,27 +357,26 @@ def create_od_routes(options, net):
             if any(route is not None for route in [coords_route, nodes_route, edges_route]):
                 route_ata_list = []
 
+                src_ata = get_ATA_from_db(sqlite3.connect(options.traffic_db), str(nodes_route[0]))
+                des_ata = get_ATA_from_db(sqlite3.connect(options.traffic_db), str(nodes_route[-1]))
                 # Check if the number of vehicles is okay for both source and destination
                 if is_n_vehicles_ok(options, src_ata, traffic_df, is_src_or_des=True) and \
                         is_n_vehicles_ok(options, des_ata, traffic_df, is_src_or_des=True):
-                    # if src_ata:
-                    #     route_ata_list.append(src_ata)
+
                     for i, node in enumerate(nodes_route):  # Exclude the last node for now
                         ata = get_ATA_from_db(sqlite3.connect(options.traffic_db), str(node))
                         if ata and ata not in route_ata_list and ata != des_ata and is_n_vehicles_ok(
                                 options, ata,
                                 traffic_df):
                             route_ata_list.append(ata)
-                    # if des_ata:
-                    #     route_ata_list.append(des_ata)
 
                     for ata in route_ata_list:
                         row_index = traffic_df.loc[traffic_df['ATA'] == ata].index
                         traffic_df.at[row_index[0], 'n_vehicles'] -= 1
-                        district = traffic_df.at[row_index[0], 'district_code']
-                        row_index_district = veh_per_district_df.loc[
-                            veh_per_district_df['district_code'] == district].index
-                        veh_per_district_df.at[row_index_district[0], 'n_vehicles'] -= 1
+                        # district = traffic_df.at[row_index[0], 'district_code']
+                        # row_index_district = veh_per_district_df.loc[
+                        #     veh_per_district_df['district_code'] == district].index
+                        # veh_per_district_df.at[row_index_district[0], 'n_vehicles'] -= 1
 
                     route_id_list.append(f'{edges_route[0]}_to_{edges_route[-1]}')
                     route_list.append(edges_route)
@@ -292,25 +416,26 @@ def create_od_routes(options, net):
                        'exec_time': exec_time_list}
     gen_routes_df = pd.DataFrame.from_dict(gen_routes_data)
     gen_routes_df.to_csv(
-        f'/home/josedaniel/Modelo_distrib_trafico_real/routes_data/gen_routes_data_{str(percentage)}p_{str(tolerated_error)}_net_edited.csv',
+        f'/home/josedaniel/Modelo_distrib_trafico_real/routes_data/gen_routes_data_{str(percentage)}p_{str(tolerated_error)}_net_edited_kr.csv',
         index=False)
     gen_routes_df.drop(columns={"exec_time", "coord"}, inplace=True)
     gen_routes_df_clean = gen_routes_df['route_id'].value_counts().to_frame().reset_index()
     gen_routes_df_clean.rename(columns={'index': 'route_id', 'route_id': 'n_vehicles'}, inplace=True)
     gen_routes_df_clean.to_csv(
-        f'/home/josedaniel/Modelo_distrib_trafico_real/veh_per_route/routes_{str(percentage)}p_{str(tolerated_error)}_net_edited.csv',
+        f'/home/josedaniel/Modelo_distrib_trafico_real/veh_per_route/routes_{str(percentage)}p_{str(tolerated_error)}_net_edited_kr.csv',
         index=False)
     traffic_df.to_csv(
-        f'/home/josedaniel/Modelo_distrib_trafico_real/traffic_data/csv/traffic_df_modified_{str(percentage)}p_{str(tolerated_error)}_net_edited.csv',
+        f'/home/josedaniel/Modelo_distrib_trafico_real/traffic_data/csv/traffic_df_modified_{str(percentage)}p_{str(tolerated_error)}_net_edited_kr.csv',
         index=False)
+
 
 def calculate_route(src_point, des_point, net, not_suitable_edges):
     src_lat, src_lon = src_point
     des_lat, des_lon = des_point
     src_x, src_y = net.convertLonLat2XY(src_lon, src_lat)
     des_x, des_y = net.convertLonLat2XY(des_lon, des_lat)
-    edges_set_start = net.getNeighboringEdges(src_x, src_y, r=250)
-    edges_set_end = net.getNeighboringEdges(des_x, des_y, r=250)
+    edges_set_start = net.getNeighboringEdges(src_x, src_y, r=20)
+    edges_set_end = net.getNeighboringEdges(des_x, des_y, r=20)
 
     if edges_set_start is not None and edges_set_end is not None and len(edges_set_start) > 0 and len(
             edges_set_end) > 0:
@@ -326,14 +451,14 @@ def calculate_route(src_point, des_point, net, not_suitable_edges):
 
             path, _ = net.getOptimalPath(net.getEdge(src_edge), net.getEdge(dst_edge),
                                          fastest=bool(random.getrandbits(1)))
+
             if path is not None:
                 final_edges = [edge.getID() for edge in path]
-                if sumolib.route.getLength(net, final_edges) < 1250:
-                    return None, None, None
-                final_nodes = [node.getID() for edge in path for node in (edge.getFromNode(), edge.getToNode())]
-                final_coords = [net.convertXY2LonLat(*net.getNode(node).getCoord()) for node in final_nodes]
+                if not sumolib.route.getLength(net, final_edges) < 1250:
+                    final_nodes = [node.getID() for edge in path for node in (edge.getFromNode(), edge.getToNode())]
+                    final_coords = [net.convertXY2LonLat(*net.getNode(node).getCoord()) for node in final_nodes]
 
-                return final_coords, final_nodes, final_edges
+                    return final_coords, final_nodes, final_edges
     else:
         return None, None, None
 
